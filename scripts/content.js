4857bb1c0bcdc63b93481c6c86a47605a97f71fc
Jasneet Dua <jasneetdua96@gmail.com>
2023-11-20 11:23:02 +0530

    added functionality to group the tabs

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,63 @@
-console.log('tab organizer loaded');
+// es import not yet supported in content scripts.
+// should be in const file
+const CONSTANTS = {
+    GROUP_EXISTANCE_CHECK: 'GROUP_EXISTANCE_CHECK',
+    MOVE_TO_GROUP: 'MOVE_TO_GROUP',
+};
+
+// general utils
+const getCookie = (cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+const getOrgIdFromCookie = () => {
+    var sid = getCookie('sid');
+    var orgId = sid ? sid.split('!')[0] : null;
+    return orgId;
+}
+
+// general utils ends here
+
+
+const init = async () => {
+    const orgId = getOrgIdFromCookie();
+    // if org id found, ask service worker to group
+    if(orgId){
+        const existanceCheckResponse = await chrome.runtime.sendMessage({
+            action: CONSTANTS.GROUP_EXISTANCE_CHECK,
+            orgId: orgId
+        });
+
+        if(existanceCheckResponse.isExisting){
+            await chrome.runtime.sendMessage({
+                action: CONSTANTS.MOVE_TO_GROUP,
+                orgId: orgId
+            });
+        }
+        else {
+            const createGroup = prompt("Create Group?", existanceCheckResponse.nameSuggestion);
+            if(createGroup !== null){
+                await chrome.runtime.sendMessage({
+                    action: CONSTANTS.MOVE_TO_GROUP,
+                    orgId: orgId, 
+                    name: createGroup
+                });
+            }
+        }
+    }
+}
+
+// content js starting point
+init();
---
+++ scripts/service_worker.js
@@ -0,0 +1,75 @@
+// es import not yet supported in content scripts.
+// should be in const file
+const CONSTANTS = {
+    GROUP_EXISTANCE_CHECK: 'GROUP_EXISTANCE_CHECK',
+    MOVE_TO_GROUP: 'MOVE_TO_GROUP',
+};
+
+const createGroup = async (currentTab, orgId, name) => {
+    // check group existance for the passed org id
+    const existingGroup = await chrome.storage.session.get([orgId]);
+    const existingGroupId = existingGroup[orgId];
+
+    if (existingGroupId) {
+        // add tab in existing group
+        await chrome.tabs.group({ tabIds: currentTab.id, groupId: existingGroupId });
+    }
+    else {
+        // create new group and add tab into the group
+        const newGroup = await chrome.tabs.group({ tabIds: currentTab.id });
+        await chrome.storage.session.set({ [orgId]: newGroup });
+        await chrome.tabGroups.update(newGroup, { title: name });
+    }
+}
+
+
+const getNameSuggestion = (origin) => {
+    const originSegments = origin.split('.');
+    if (originSegments.length) {
+        const instanceName = originSegments[0];
+        const indexOfProtocolSlashes = instanceName.indexOf('//');
+        if (indexOfProtocolSlashes !== -1) {
+            return instanceName.substring(indexOfProtocolSlashes + 2);
+        }
+        else {
+            return instanceName;
+        }
+    }
+    return 'Salesforce';
+}
+
+
+// new tab listener
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    const nameSuggestion = getNameSuggestion(sender.origin);
+
+    if (request.action === CONSTANTS.MOVE_TO_GROUP) {
+        const currentTab = sender.tab;
+        const orgId = request.orgId;
+        const name = !!request.name ? request.name : nameSuggestion;
+        createGroup(currentTab, orgId, name);
+    }
+    else if (request.action === CONSTANTS.GROUP_EXISTANCE_CHECK) {
+        const orgId = request.orgId;
+        (async () => {
+            const existingGroup = await chrome.storage.session.get([orgId]);
+            const existingGroupId = existingGroup[orgId];
+
+            sendResponse({
+                isExisting: !!existingGroupId,
+                nameSuggestion: nameSuggestion
+            });
+        })();
+        return true;
+    }
+});
+
+// group removal listener
+chrome.tabGroups.onRemoved.addListener(async (removedGroup) => {
+    const storageMap = await chrome.storage.session.get();
+    const orgId = Object.keys(storageMap)[Object.values(storageMap).indexOf(removedGroup.id)];
+    if (orgId) {
+        await chrome.storage.session.remove(orgId);
+    }
+});
